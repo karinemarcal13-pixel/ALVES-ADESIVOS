156de0b7582aa88c3adeedf0ae439ccf8def33cc
karinemarcal13-pixel <karinemarcal13@gmail.com>
2026-03-26 20:45:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-function enviarWhats(event) {
-  event.preventDefault();
+// WhatsApp
+function chamarWhats() {
+  let numero = "5599999999999"; // seu número
+  let msg = "Olá! Gostaria de um orçamento.";
 
-  const nome = document.getElementById("nome").value.trim();
-  const servico = document.getElementById("servico").value.trim();
-
-  if (!nome || !servico) {
-    alert("Preencha os campos");
-    return;
-  }
-
-  const mensagem = `Olá, meu nome é ${nome}.
-Gostaria de um orçamento.
-
-📍 Londrina e região
-Serviço: ${servico}`;
-
-  window.open(`https://wa.me/5543984046323?text=${encodeURIComponent(mensagem)}`);
+  window.open(`https://wa.me/${numero}?text=${encodeURIComponent(msg)}`);
 }
+
+// ANIMAÇÃO AO SCROLL
+let elementos = document.querySelectorAll(".fade");
+
+window.addEventListener("scroll", () => {
+  elementos.forEach(el => {
+    let pos = el.getBoundingClientRect().top;
+    if (pos < window.innerHeight - 100) {
+      el.classList.add("show");
+    }
+  });
+});
